9a116cdb577061042235c83ce0687cb1394ea1e3
typhoon0678 <typhoon0678@gmail.com>
2024-04-14 13:24:48 +0900

    React Router 세팅

--- frontend/mall/src/router/root.js
+++ frontend/mall/src/router/root.js
@@ -1,10 +1,13 @@
 import {lazy, Suspense} from "react";
 import {createBrowserRouter} from "react-router-dom";
+import todoRouter from "./todoRouter";
 
 
 const Loading = <div className={'bg-red-900'}>Loading...</div>
+
 const Main = lazy(() => import("../pages/MainPage"))
 const About = lazy(() => import("../pages/AboutPage"))
+const TodoIndex = lazy(() => import("../pages/todo/IndexPage"))
 
 const root = createBrowserRouter([
 
@@ -15,6 +18,11 @@ const root = createBrowserRouter([
     {
         path: "about",
         element: <Suspense fallback={Loading}><About/></Suspense>
+    },
+    {
+        path: "todo",
+        element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
+        children: todoRouter()
     },
 
 ])
